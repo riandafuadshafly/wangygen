f4c27b013ed89701b637d242dd57aad7900baba1
Rianda ID <44912483+riandafuadshafly@users.noreply.github.com>
2021-05-25 03:00:22 +0700

    Add files via upload

---
+++ wangy.sh
@@ -0,0 +1,57 @@
+﻿#!/bin/bash
+#version 1.0
+#wibuwangy
+#just for fun
+
+#25/05/2021
+
+clear
+# Variables
+b='\E[34m'
+r='\E[31m'
+c='\E[36m'
+g='\E[32m'
+y='\E[33m'
+
+
+sleep 3
+clear
+echo ""
+echo -e " Author   : Rianda Fuad Shafly "
+echo -e " Info     : WibuWangy Generator"
+echo -e " Website  : www.riandafuadshafly.my.id "
+echo -e " Contact  : bangrianda456@gmail.com "
+echo ""
+echo -e $c "
+Masukkan nama husbu/waifu : "
+read waifu
+echo -e $y " Daftar List : "
+echo -e $y "   1. Genjot "
+echo -e $y "   2. Jilat "
+echo -e $y "   3. Wangy Wangy"
+echo -e $y "   4. Watashi"
+read -p  " Pilih salah satu angka diatas = " wibu;
+
+case $wibu in
+1)
+echo -e $g "Buruan, panggil gue SIMP, ato BAPERAN. ini MURNI PERASAAN GUE. Gue pengen genjot bareng $waifu. Ini seriusan, suaranya yang imut, mukanya yang cantik, apalagi badannya yang aduhai ningkatin gairah gue buat genjot $waifu. Setiap lapisan kulitnya pengen gue jilat. Saat gue mau crot, gue bakal moncrot sepenuh hati, bisa di perut, muka, badan, teteknya, sampai lubang burit pun bakal gue crot sampai puncak klimaks. Gue bakal meluk dia abis gue moncrot, lalu nanya gimana kabarnya, ngrasain enggas bareng saat telanjang. Dia bakal bilang kalau genjotan gue mantep dan nyatain perasaannya ke gue, bilang kalo dia cinta ama gue. Gue bakal bilang balik seberapa gue cinta ama dia, dan dia bakal kecup gue di pipi. Terus kita ganti pakaian dan ngabisin waktu nonton film, sambil pelukan ama makan hidangan favorit. Gue mau $waifu jadi pacar, pasangan, istri, dan idup gue. Gue cinta dia dan ingin dia jadi bagian tubuh gue. Lo kira ini copypasta? Kagak cok. Gue ngetik tiap kata nyatain prasaan gue. Setiap kali elo tanya dia siapa, denger ini baik-baik : DIA ISTRI GUE. Gue sayang $waifu, dan INI MURNI PIKIRAN DAN PERASAAN GUE.  "
+
+;;
+
+2)
+echo -e $g "GW BENAR-BENAR PENGEN JILATI KAKI $waifu. GW PENGEN BANGET MENJILAT SETIAP BAGIAN KAKINYA SAMPAI AIR LIUR GW BERCUCURAN KAYAK AIR KERINGAT LALU NGENOD DENGANNYA SETIAP HARI SAMPAI TUBUH KITA MATI RASA YA TÜHAN. GW INGIN MEMBUAT ANAK-ANAK DENGAN $waifu SEBANYAK SATU TÌM SEPAK BOLA DAN MEMBUAT SATU TIM SEPAK BOLA LAINNYA UNTUK MELAWAN ANAK-ANAK TIM SEPAK BOLA PERTAMA GW YANG GW BUAT SAMA $waifu. GW PENGEN MASUK KE SETIAP LUBANG TUBUHNYA, MAU ITU LUBANG HIDUNG, LUBANG TELINGA, RONGGA MATA MAUPUN PUSAR, KECUALI PORI-PORI KULIT. KEMUDIAN GW AKAN MENJADIKANNYA MANUSIA YANG TIDAK BISA HIDUP KALO TIDAK GW KENTOG SETIAP HARI. DAN KALAU GUA DISEPONG GUA RELA KONTL GUA PUTUS. "
+
+;;
+
+3)
+echo -e $g "$waifu $waifu $waifu ❤️ ❤️ ❤️ WANGI WANGI WANGI WANGI HU HA HU HA HU HA, aaaah baunya rambut $waifu wangi aku mau nyiumin aroma wanginya $waifu AAAAAAAAH ~ Rambutnya.... aaah rambutnya juga pengen aku elus-elus ~~~~ AAAAAH $waifu keluar pertama kali di anime juga manis ❤️ ❤️ ❤️ banget AAAAAAAAH $waifu AAAAA LUCCUUUUUUUUUUUUUUU............ $waifu AAAAAAAAAAAAAAAAAAAAGH ❤️ ❤️ ❤️apa ? $waifu itu gak nyata ? Cuma HALU katamu ? nggak, ngak ngak ngak ngak NGAAAAAAAAK GUA GAK PERCAYA ITU DIA NYATA NGAAAAAAAAAAAAAAAAAK PEDULI BANGSAAAAAT !! GUA GAK PEDULI SAMA KENYATAAN POKOKNYA GAK PEDULI. ❤️ ❤️ ❤️ $waifu gw ... $waifu di laptop ngeliatin gw, $waifu .. kamu percaya sama aku ? aaaaaaaaaaah syukur $waifu aku gak mau merelakan $waifu aaaaaah ❤️ ❤️ ❤️ YEAAAAAAAAAAAH GUA MASIH PUNYA $waifu SENDIRI PUN NGGAK SAMA AAAAAAAAAAAAAAH "
+
+;;
+
+4)
+echo -e $g "watashi mencintai $waifu dengan tulus dan penuh kasih sayang, watashi tidak tahan dengan hinaan kalian yg kalian berikan terhadap '+nama+'. '+nama+' selalu menangis dikamarnya setiap malam karena hinaan kalian. "shine, shine, shine" hanya kata itulah yang ada dipikiran watashi tpi watashi hanya manusia lemah yang tak berdaya jika dikroyok banyak orang. Suatu saat kemudian ada orang biadab memfitnah $waifu dengan membuat video skandal lalu menyebarkannya di website pornografi. Amarah dan aura merah menyelimuti watashi tanpa disadari darah menetes dari mata watashi secepat kilat watashi menengok cermin lalu watashi melihat rambut watashi belahan menjadi putih lalu ada kagune di punggung watashi lalu sosok kaneki muncul dari dalam cermin tanpa berkata apapun dia memberikan maskernya dan langsung pergi melompati jendela. Watashi langung mencuci masker itu karena bau tengik mulut kaneki membekas di masker itu. Perut Watashi tiba tiba merasa lapar, watashi mencoba indomie buatan $waifu lalu watashi muntah muntah 😖, lalu terlintas dipikiran watashi jadi berita itu benar ! ghoul memang harus memakan manusia karena watashi masih mempunya jiwa manusia akhirnya watashi memakan tangan sendiri. Meskipun tidak menghilangkan rasa lapar setidaknya ini cukup untuk berdiri lalu memangsa siapapun yang menghina istri watashi. no mercy no cry u must die, watashi tak segan segan membunuh kalian jika kalian menghina $waifu "
+
+;;
+
+*) echo "Maaf, Input yang kamu masukkan tidak ada "
+esac
